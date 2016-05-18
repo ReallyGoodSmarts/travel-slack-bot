2bf2a812a21a379e64cd4466723afb3fd0f21ebc
John Keefe <jkeefe@wnyc.org>
2016-05-17 21:29:18 -0400

    adding cron stuff

---
+++ cron.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Tell cron to use my local .profile information
+source /home/ubuntu/.profile
+
+# change directories into the /lunchbot
+cd /home/ubuntu/bothouse/travel-slack-bot
+
+# run the bot using node
+node index.js
+
+#### Also ...
+
+# edit crontab with 
+#    crontab -e
+# and add this line ...
+#    0 9 * * * /home/ubuntu/bothouse/travel-slack-bot/cron.sh > /home/ubuntu/bothouse/travel-slack-bot/cron.log
